7a4ba9bd79f4171b674b3e015a9106b2cd209a45
fedizarai <fedi1920@hotmail.com>
2021-03-18 15:34:02 +0100

    traveller edited

--- AsteroidMiner/src/CoreClasses/Traveler.java
+++ AsteroidMiner/src/CoreClasses/Traveler.java
@@ -1,5 +1,42 @@
 package CoreClasses;
 
-public class Traveler {
+public abstract class Traveler {
+    private boolean hidden;
+    private final Place currentPlace;
 
+
+    public Traveler(boolean hidden, Place currentPlace) {
+        this.hidden = hidden;
+        this.currentPlace = currentPlace;
+    }
+
+    public boolean getHidden() {
+        return hidden;
+    }
+
+    public void setHidden(boolean hidden) {
+        this.hidden = hidden;
+    }
+
+    public void hide() {
+
+        if (currentPlace.hollow() == true && !hidden)
+            hidden = true;
+        System.out.println("You can't hide");
+    }
+
+    public void unhide() {
+        if (!hidden)
+            System.out.println("already not hidden");
+        hidden = false;
+
+    }
+
+    public void teleport(TeleportationGate tg) {
+    }
+
+
+    public abstract void drill();
+
+    public abstract void dying();
 }
