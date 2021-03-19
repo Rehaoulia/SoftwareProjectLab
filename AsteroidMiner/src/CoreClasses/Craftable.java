29cec0748f88de2c45f0b93c4a9e00e6bec52e73
Amani215 <amanibrik5@gmail.com>
2021-03-19 02:12:05 +0100

    feat: create robot and craftable

--- AsteroidMiner/src/CoreClasses/Craftable.java
+++ AsteroidMiner/src/CoreClasses/Craftable.java
@@ -1,5 +1,23 @@
 package CoreClasses;
 
 public interface Craftable {
-
+	//////////////////////////////////////////////////////////////////////
+	//RequiredMinerals should be strings. having them as minerals does not make sense.
+	//////////////////////////////////////////////////////////////////////
+	String[] RequiredMinerals = {};
+	Asteroid currentAsteroid = new Asteroid();
+	
+	boolean isBuilt = false;	//why is this useful? (maybe it should only be in the space station
+	
+	//returns the required materials of the craftable object
+	String[] getRequiredMinerals ();//////////////////////////////////same
+	//returns the current Asteroid of the object
+	Asteroid getCurrentAstroid ();
+	//sets the current asteroid of the object to a 
+	void setAsteroid(Asteroid a);
+	//checks if the mineral is required
+	boolean checkMineral(Mineral mineral);
+	
+	//what does this function check????
+	boolean checkStatus();
 }
