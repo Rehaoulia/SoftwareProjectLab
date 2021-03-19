4303d5cd200f784d64cb65e3442475b8a992f1f7
samer.bahi98 <samer.bahi@ieee.org>
2021-03-19 15:38:10 +0100

    feat: Sunstorm

--- AsteroidMiner/src/CoreClasses/Sunstorm.java
+++ AsteroidMiner/src/CoreClasses/Sunstorm.java
@@ -1,18 +1,29 @@
 package CoreClasses;
 
 public class Sunstorm {
+    // Attributes
 
-    // METHODS
+    private float lastStormTime;
+    private Timer timer;
+    private double wavelength;    
+    private boolean isHappening;
+
+    // Methods
     
+    public Sunstorm(double wavelength){
+        this.wavelength = wavelength;
+    }
+
     private double calculateTimeLeft(){
 
         return 0.0;
     }
     public void behave(){
-
+        
     }
 
     public void setHappening(boolean value){
+        isHappening = value;
 
     }
     public void display(){
