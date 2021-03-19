002cf28e5d4d4b692ecc71737d1b60cf4393ad9a
P4n4r0ma <61463482+nomadServer@users.noreply.github.com>
2021-03-19 16:44:53 +0100

    TeleGate set

--- AsteroidMiner/src/CoreClasses/TeleportationGate.java
+++ AsteroidMiner/src/CoreClasses/TeleportationGate.java
@@ -1,5 +1,26 @@
 package CoreClasses;
 
 public class TeleportationGate {
+	
+	private boolean deployed;
+	private TeleportationGate pairedGate;
+	
+	public TeleportationGate(){
+		deployed = false;
+	}
+
+	
+	public TeleportationGate getPairedGate() {
+		return (deployed)? pairedGate: null;
+	}
+	
+	public void setPairGate(TeleportationGate pair) {
+		if(deployed == false && pair.isPaired()==false ) {				
+		pairedGate = pair;						
+		pairedGate.deployed =true ;				// paired gate connot have another pair !
+		deployed = true;						// otherwise it turns to be recursive 
+		}
+	}
+	public boolean isPaired() {return deployed; }
 
 }
