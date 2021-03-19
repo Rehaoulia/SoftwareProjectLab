6a5683e6991828b57d850154a7a75439b92b63c2
rehaoulia <rehaoulia_med@hotmail.com>
2021-03-19 12:50:36 +0100

    Implemented Place class

--- AsteroidMiner/src/CoreClasses/Place.java
+++ AsteroidMiner/src/CoreClasses/Place.java
@@ -1,5 +1,16 @@
 package CoreClasses;
 
-public class Place {
+import java.util.ArrayList;
 
+public class Place {
+	
+	private ArrayList<Traveler> currentTravelers;
+	
+	public void addTraveler(Traveler traveler) {
+		this.currentTravelers.add(traveler);
+	}
+	
+	public void removeTraveler(Traveler traveler) {
+		this.currentTravelers.removeIf(t -> t.equals(traveler));
+	}
 }
