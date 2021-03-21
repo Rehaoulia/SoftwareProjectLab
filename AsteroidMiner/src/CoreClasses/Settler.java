b8abbb9fbc41ce836c568a82ad93ab49a4753189
rehaoulia <rehaoulia_med@hotmail.com>
2021-03-21 12:12:01 +0100

    CHnages and fixes

--- AsteroidMiner/src/CoreClasses/Settler.java
+++ AsteroidMiner/src/CoreClasses/Settler.java
@@ -1,6 +1,5 @@
 package CoreClasses;
 
-import java.sql.Date;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -13,8 +12,9 @@ public class Settler extends Traveler implements Craftable
 	private long timeOfDeath;
 	private boolean dead;
 	
-	public Settler(){
-		
+	public Settler(Asteroid startingAsteroid, String name){
+		this.currentAsteroid = startingAsteroid;
+		this.name = name;
 	}
 	 //Change currentPlace to currentAsteroid because currentPlace does not have methods such as getsDrill, getsFill, getsMine
 	public void setAsteroid(Asteroid asteroid) {
@@ -28,8 +28,8 @@ public class Settler extends Traveler implements Craftable
 	//Add deploy method to gates (basically just a setter for the deployed attribute)
 	public void putGate(TeleportationGate gate) {
 		for(TeleportationGate g : this.gates) {
-			if(gate.equals(g)) {
-				g.deploy();
+			if(gate.equals(g) && !gate.getDeployed()) {
+				g.deploy(this.currentAsteroid);
 			}
 		}
 	}
@@ -37,53 +37,48 @@ public class Settler extends Traveler implements Craftable
 	@Override
 	public void drill() {
 		this.currentAsteroid.getsDrill();
+		Controller.updateAsteroid();
 	}
 	
 	public void mine() {
-		if(!this.currentAsteroid.hollow() && this.currentAsteroid.drillable()) {
-			this.minedMinerals.add(this.currentAsteroid.getMineral());
+		if(this.currentAsteroid.getsMine()!= null && this.getCapacityLeft() > 0) {
+			this.minedMinerals.add(this.currentAsteroid.getsMine().toString());
 		}
+		Controller.updateAsteroid();
 	}
 	
 	public void fill(String mineral) {
-		if(this.currentAsteroid.hollow()==true) {
-			for(String min : minedMinerals) {
-				if(min.equals(mineral)) {
-					this.currentAsteroid.getsFill(mineral);
-				}
-			}
-		}
+		if(minedMinerals.contains(mineral)) {
+			this.currentAsteroid.getsFill(mineral);
+		}	
+		Controller.updateAsteroid();
 	}
 	
 	//timeOfDeath changed into long and revive just calculates the difference between current time and time of death also revive return type changed into boolean
 	public boolean revive(Settler S) {
-		long elapsedTime = System.currentTimeMillis() - S.timeOfDeath;
+		long elapsedTime = System.currentTimeMillis() - S.getTimeOfDeath();
 		if(elapsedTime < 10000) {
-			S.timeOfDeath = 0;
+			S.setTimeOfDeath(0);
+			Controller.updateSettler();
 			return true;
 		}else {
 			return false;
 		}
-		
 	}
 	
 	
 	//minedMinerals needs to be a string arrayList to allow calling method containsAll(), or else this part of the code will be much more complicated
-	public boolean checkRequiredMaterial(String craftable) {
+	public boolean checkRequiredMaterial(int craftable) {
 		boolean canCraft = false;
+		//remove material from settler
 		switch(craftable) {
-			case "Robot":
+			case 1:
 				if(minedMinerals.containsAll(Arrays.asList("Uranium", "Carbon", "Iron"))) {
 					canCraft = true;
 				}
 				break;
-			case "TeleportationGate":
+			case 2:
 				if(minedMinerals.containsAll(Arrays.asList("Uranium", "WaterIce", "Iron", "Iron"))) {
-					canCraft = true;
-				}
-				break;
-			case "SpaceStation":
-				if(minedMinerals.containsAll(Arrays.asList("Uranium", "Uranium", "Uranium", "Carbon", "Carbon", "Carbon", "Iron", "Iron", "Iron", "WaterIce", "WaterIce", "WaterIce"))) {
 					canCraft = true;
 				}
 				break;
@@ -92,33 +87,47 @@ public class Settler extends Traveler implements Craftable
 	}
 	
 	//By making this method return craftable type we can get rid of craftRobot(), craftTeleportationGate(), craftSpaceStation()
-	public Craftable craft(String craftable) {
+	public Craftable craft(int craftable) {
 		Craftable object = null;
 		if(checkRequiredMaterial(craftable)) {
 			switch(craftable) {
-			case "Robot":
-				object = new Robot();
+			case 1:
+				object = new Robot(this.currentAsteroid);
+				minedMinerals.removeAll(Arrays.asList("Uranium", "Carbon", "Iron"));
 				break;
-			case "TeleportationGate":
-				object = new TeleportationGate();
+			case 2:
+				//check if you already have teleportation gates
+				// add to gates attribute
+				if(gates == null) {
+					gates = new TeleportationGate[2];
+					gates[0] = new TeleportationGate();
+					gates[1] = new TeleportationGate();
+					minedMinerals.removeAll(Arrays.asList("Uranium", "WaterIce", "Iron", "Iron"));
+				}
 				break;
-			case "SpaceStation":
-				object = new SpaceStation();
+			case 3 :
+				if(this.currentAsteroid.getSpaceStation()!=null) {
+					object = new SpaceStation(this.currentAsteroid);
+				}
 				break;
-		}
+			}
 		}
 		return object;
 	}
 	
 	//Return type changed to string instead of craftable
 	public String showCraftMenu() {
-		return null;
+		return "1-Robot \n2-Teleportation Gate\n3-SpaceStation ";
 	}
 	
 	//Removed pairGates() method as gates are paired by default
 	
-	public void setTimeOfDeath(){
+	private void setTimeOfDeath(){
 		this.timeOfDeath = System.currentTimeMillis();
+	}
+	
+	public void setTimeOfDeath(long time){
+		this.timeOfDeath = time;
 	}
 	
 	//might be useless
@@ -126,7 +135,8 @@ public class Settler extends Traveler implements Craftable
 		return timeOfDeath;
 	}
 	
-	public void setDeath() {
+	@Override
+	public void die() {
 		this.dead = true;
 	}
 	
@@ -134,7 +144,21 @@ public class Settler extends Traveler implements Craftable
 		return this.dead;
 	}
 	
+	//add to mine
 	public int getCapacityLeft() {
 		return 10 - this.minedMinerals.size();
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+
+	public void dying() {
+		this.setTimeOfDeath();
+	}
+	public void hide() {
+		if(this.currentAsteroid.getHide() == true) {
+			this.hidden = true;
+		}
 	}
 }
