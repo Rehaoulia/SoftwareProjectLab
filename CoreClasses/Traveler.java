cfe5cf67679c5a5dd9cf7e8d1d448545cc04f610
nomadServer <61463482+nomadServer@users.noreply.github.com>
2021-04-15 22:51:30 +0200

    fresh teleGate done

---
+++ CoreClasses/Traveler.java
@@ -0,0 +1,61 @@
+
+
+package CoreClasses;
+
+//import com.jme3.math.Vector3f;
+//import com.jme3.scene.Spatial;
+
+public abstract class Traveler {
+    protected boolean hidden;
+    protected Place currentPlace;
+    //private Spatial model;
+   // private Vector3f location;
+
+    // public Traveler(Vector3f loc) {
+    //     location = loc ;
+    // }
+
+    
+    //  public void setModel(Spatial mod) {
+    //      mod.setLocalTranslation(location);
+    //     model = mod;
+    // }
+     
+    // public Spatial getModel(){
+    //     model.setLocalTranslation(location);
+    //     return model ;
+    //  }
+    
+    // public void setLocation(Vector3f loc){
+    
+    //     location = loc ;
+    // }
+  
+    //  public Vector3f getLocation(){
+    
+    //    return location ;
+    // }
+
+
+  
+    public boolean getHidden() {
+        return hidden;
+    }
+
+    public void setHidden(boolean hidden) {
+        this.hidden = hidden;
+    }
+
+    public void unhide() {
+        setHidden(false);
+
+    } 
+
+
+
+    public abstract void hide() throws InterruptedException;
+
+    public abstract void drill();
+
+    public abstract void die();
+}
