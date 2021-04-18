8103e53d506532495e62f5dde7e2eaa72a4e1a9c
Rehaoulia <rehaoulia_med@hotmail.com>
2021-04-18 23:56:47 +0200

    Hama's Tests

---
+++ AsteroidMiner/src/test/java/CoreClasses/MovementTest.java
@@ -0,0 +1,26 @@
+package CoreClasses;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class MovementTest {
+
+	static Controller c = new Controller();
+    static Settler S;
+    static int nextAsteroid;
+    
+    public static void setUpTest(){
+        String[] names = {"samer"};
+        c.startGame(names);
+        S  = c.getSettlers().get(0);
+    }
+    
+    @Test
+    public void TravelTest() {
+    	setUpTest();
+    	nextAsteroid =  S.getAsteroid().getID() + 1;
+    	S.travel(c.asteroids.get(nextAsteroid));
+    	assertTrue("S successfully moved to asteroid A"+nextAsteroid, S.getAsteroid().getID() == nextAsteroid);
+    }
+}
