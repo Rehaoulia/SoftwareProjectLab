1cb15c73b99bd237ce12511f8ffbe2955f788610
Rehaoulia <53380919+Rehaoulia@users.noreply.github.com>
2021-04-19 11:04:07 +0200

    Update InitializationTest.java

--- AsteroidMiner/src/test/java/CoreClasses/InitializationTest.java
+++ AsteroidMiner/src/test/java/CoreClasses/InitializationTest.java
@@ -3,6 +3,7 @@ package CoreClasses;
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
+//Test if the settler and asteroids have been created properly
 public class InitializationTest {
 	static Controller c = new Controller();
 	static int numberofSettlers = 2;
@@ -11,14 +12,14 @@ public class InitializationTest {
 	@Test
 	public void PlayersCreated() {
 		String[] names = {"S1", "S2"};
-		c.startGame(names);
-		assertTrue("Players have been sucessfully created", numberofSettlers == c.getSettlers().size());
+		c.startGame(names); //Creates two players and initializes environment
+		assertTrue("Players have been sucessfully created", numberofSettlers == c.getSettlers().size()); //checks if the two players have been created
 	}
 	
 	@Test
 	public void AsteroidsCreated() {
-		c.setupGame();
-		numberofAsteroids = Controller.asteroids.size();
-		assertTrue(numberofAsteroids + " Asteroids Created", numberofAsteroids >= 40 && numberofAsteroids <= 50);
+		c.setupGame(); //creates asteroids
+		numberofAsteroids = Controller.asteroids.size(); //gets the number of created asteroids from controller class
+		assertTrue(numberofAsteroids + " Asteroids Created", numberofAsteroids >= 40 && numberofAsteroids <= 50); //checks if  the number of created asteroids is between 40 and 50
 	}
 }
