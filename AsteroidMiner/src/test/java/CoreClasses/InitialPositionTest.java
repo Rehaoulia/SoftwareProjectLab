9fda60cacaf34f73f463a109d14f50e95a9b6ccc
Rehaoulia <53380919+Rehaoulia@users.noreply.github.com>
2021-04-19 11:01:39 +0200

    Update InitialPositionTest.java

--- AsteroidMiner/src/test/java/CoreClasses/InitialPositionTest.java
+++ AsteroidMiner/src/test/java/CoreClasses/InitialPositionTest.java
@@ -3,14 +3,15 @@ package CoreClasses;
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
+//Testing if the Settler will get an initial asteroid when created
 public class InitialPositionTest {
 	static Controller c = new Controller();
 	
 	@Test
 	public void InitPositionTest() {
 		String[] names = {"S"};
-		c.startGame(names);
+		c.startGame(names); //Creates settler and initializes asteroids
 		Settler set = c.settlers.get(0);
-		assertTrue("Settler has an initial position", set.getAsteroid() != null);
+		assertTrue("Settler has an initial position", set.getAsteroid() != null); //checks if the settlers current asteroid is not null
 	}
 }
