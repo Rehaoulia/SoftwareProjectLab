1f25752df8dfd259df427b1c5e9b68534c3861e2
fedizarai <78793170+fedizarai@users.noreply.github.com>
2021-04-19 01:42:55 +0100

    Fedy's test

--- AsteroidMiner/src/test/java/CoreClasses/RobotTest.java
+++ AsteroidMiner/src/test/java/CoreClasses/RobotTest.java
@@ -1,5 +1,7 @@
 package CoreClasses;
 
+package CoreClasses;
+
 import static org.junit.Assert.assertTrue;
 import CoreClasses.Robot;
 import CoreClasses.Asteroid;
@@ -10,27 +12,59 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class RobotTest 
-{
-    
+public class RobotTest {
+
     /**
      * Rigorous Test :-)
      */
     static Controller c = new Controller();
     static Robot R;
-    public static void setUpTest(){
-        String[] names = {"samer"};
+    static Asteroid a;
+    static SpaceStation S;
+    static Settler Settler;
+
+    public static void setUpTest() {
+        String[] names = { "S1" };
         c.startGame(names);
-        R = new Robot(c.asteroids.get(4));
+        Settler = c.getSettlers().get(0);
+        S = (SpaceStation) Settler.craft(2, c);
+
     }
 
     @Test
-    public void TravelTest()
-    {
+    public void SublimeTest() {
         setUpTest();
-        int nextAsteroid = R.getCurrentAstroid().getID()+1;
-        R.travel(c.asteroids.get(nextAsteroid));
-        assertTrue("Success: Robot R1 moved to asteroid A"+nextAsteroid, nextAsteroid == 5);
-        
+        int t = Controller.getSublimingAsteroids().get(0);
+        Controller.asteroids.get(t).setPerihelion(true);
+        c.Sublime(c, t);
+        assertTrue("Water Ice sublimed successfully", Controller.asteroids.get(t).hollow() == true);
+
     }
+
+    @Test
+    public void ExplodeTest() {
+        setUpTest();
+        int t = Controller.getExplodingAsteroids().get(0);
+        Controller.asteroids.get(t).setPerihelion(true);
+        c.Explode(c, t);
+        assertTrue("The asteroid " + t + "explode successfully ", c.settlers.get(0).getDeath() == true);
+
+    }
+
+    @Test
+    public void winGameTest() {
+        setUpTest();
+        c.checkSpaceStation(S.getID());
+        assertTrue("you won ", c.getWin() == true);
+
+    }
+
+    @Test
+    public void SettlerHidingTest() {
+        setUpTest();
+        c.settlers.get(0).hide();
+        assertTrue("you are hidden' ", c.settlers.get(0).hidden);
+
+    }
+
 }
